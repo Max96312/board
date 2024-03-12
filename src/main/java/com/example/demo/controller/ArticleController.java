8e142a83c0accdcbede32e0e722539476af0e53d
max96312 <k0mp4ny16@gmail.com>
2024-03-12 17:12:44 +0900

    fetch: html 목업 페이지 작업

---
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *  /articles
+ *  /articles/{article-id}
+ *  /articles/search
+ *  /articles/search-hashtag
+ */
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+    @GetMapping
+    public String articles(){
+        return "articles/index";
+    }
+}
