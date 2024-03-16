8c363834f731a3f7aebec6356a1a13aeae5e34ea
max96312 <k0mp4ny16@gmail.com>
2024-03-16 16:14:57 +0900

    feat:단일 게시글 페이지 목업 추가

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -17,5 +18,10 @@ public class ArticleController {
     @GetMapping
     public String articles(){
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId){
+        return "articles/detail";
     }
 }
