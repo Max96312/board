8ae3f0c33ac9196a67d7b8ff6205f351cccd492a
max96312 <k0mp4ny16@gmail.com>
2024-03-17 23:29:10 +0900

    feat: ArticleComment 관련 dto 추가, service 골격 잡기

---
+++ src/main/java/com/example/demo/service/ArticleCommentService.java
@@ -0,0 +1,23 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.ArticleCommentDto;
+import com.example.demo.repository.ArticleCommentRepository;
+import com.example.demo.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(Long articleId){
+        return List.of();
+    }
+}
