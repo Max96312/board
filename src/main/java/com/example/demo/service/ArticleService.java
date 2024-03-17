de68f6d7f5f940673a178f275608dd18f9d79fce
max96312 <k0mp4ny16@gmail.com>
2024-03-17 23:28:49 +0900

    feat: Article 관련 dto & SearchType 타입 추가, service 골격 잡기

---
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -0,0 +1,38 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.type.SearchType;
+import com.example.demo.dto.ArticleDto;
+import com.example.demo.dto.ArticleUpdateDto;
+import com.example.demo.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType title, String searchKeyword) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(Long id) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+}
