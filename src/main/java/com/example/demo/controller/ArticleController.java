e4be842108bbed0cf2b3f7bf380be011e36c5962
max96312 <k0mp4ny16@gmail.com>
2024-03-20 12:34:45 +0900

    feat: 게시판 서브시에 필요한 비즈니스 로직 구현 mvc 패턴에 따른 게시판 페이지 기능 구현

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -1,9 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.type.SearchType;
+import com.example.demo.dto.ArticleResponse;
+import com.example.demo.dto.ArticleWithCommentsResponse;
+import com.example.demo.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *  /articles
@@ -11,17 +21,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *  /articles/search
  *  /articles/search-hashtag
  */
+
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
+    private final ArticleService articleService;
+
     @GetMapping
-    public String articles(){
+    public String articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map
+            ){
+        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+
         return "articles/index";
     }
 
     @GetMapping("/{articleId}")
-    public String article(@PathVariable Long articleId){
+    public String article(@PathVariable Long articleId, ModelMap map){
+        ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+        map.addAttribute("article", article);
+        map.addAttribute("articleComments", article.articleCommentsResponses());
+
         return "articles/detail";
     }
 }
